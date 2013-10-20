a356628e8d99c3805e042a13f4bba5bebff73ac7
Uri Shaked <uri@salsa4fun.co.il>
2013-10-20 22:37:33 +0200

    Testing auto deploy

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Auto deploy test 1!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
